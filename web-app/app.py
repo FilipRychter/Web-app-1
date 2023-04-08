fd23c24d1bbc13863bfccae6354dd7ac5cd339a1
Filip-Rychter <filopr2000@gmail.com>
2023-04-08 06:25:04 +0200

    commit-2

--- web-app/app.py
+++ web-app/app.py
@@ -1,3 +1,22 @@
-#TO jest test czy uda misie to ...
+from flask import Flask, render_template, request, redirect, url_for
 
-#Kolejny test tym razem bez commit
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/sign-up')
+def sign_up():
+    return render_template('sign-up.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
